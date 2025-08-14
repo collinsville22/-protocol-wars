702795466951a46ca505631678b9ea9410d3ae4f
Ezewuzie Chiagoziem Collins <107811375+collinsville22@users.noreply.github.com>
2025-08-14 11:17:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
-}
+  const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {},
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false
+      };
+      return config;
+    },
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
